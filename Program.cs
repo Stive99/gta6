fb915d45353c644710a28758445e492275155308
Stive_99 <aslanych99@mail.ru>
2025-01-05 17:21:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace gta6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Process.Start(new ProcessStartInfo
+				{
+					FileName = "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+					UseShellExecute = true
+				});
+
+				Console.WriteLine("Ссылка успешно открыта.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Ошибка: {ex.Message}");
+			}
+
+			Environment.Exit(0);
+		}
+	}
+}
